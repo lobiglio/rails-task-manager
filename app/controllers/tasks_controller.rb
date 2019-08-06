f840f6caf04a92d3a43d5284e01d6f01a583ba20
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-06 15:36:25 +0200

    Exercice done, design need to be improved

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,9 +17,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
